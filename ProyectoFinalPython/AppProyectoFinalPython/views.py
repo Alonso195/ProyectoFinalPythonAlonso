499da5f8d9ded45296b8d60291017596575e2e0b
Jorge Alonso Rojas Perez <jarojasp@ccss.sa.cr>
2022-08-15 18:37:29 -0600

    Login

--- ProyectoFinalPython/AppProyectoFinalPython/views.py
+++ ProyectoFinalPython/AppProyectoFinalPython/views.py
@@ -1,7 +1,12 @@
 
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.views.generic import ListView, DeleteView, CreateView, UpdateView, DetailView
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 from AppProyectoFinalPython.models import Contact, Proveedor,Cliente,Articulo
 from AppProyectoFinalPython.forms import ContactFormulario, ProveedorFormulario,ClienteFormulario,ArticuloFormulario
@@ -41,20 +46,54 @@ def buscarProveedor(request):
 
     return HttpResponse(respuesta)
 
+def login_request(request):
+    print('method:', request.method)
+    print('post:', request.POST)
+    if request.method == 'POST':
+
+        miFormulario = AuthenticationForm(request, data=request.POST)
+
+        if miFormulario.is_valid():
+
+            data = miFormulario.cleaned_data
+
+            usuario = data["username"]
+            psw = data["password"]
+
+            user = authenticate(username=usuario, password=psw)
+
+            if user:
+
+                login(request, user)
+
+                return render(request, "inicio.html", {"mensaje": f'Bienvenido {usuario}'})
+
+            else:
+
+                return render(request, "login.html", {"mensaje": "Error, datos incorrectos"})
+
+        return render(request, "login.html", {"mensaje": "Error, datos incorrectos"})
+
+    else:
+
+        miFormulario = AuthenticationForm()
+
+    return render(request, "login.html", {"miFormulario": miFormulario})
+
 #------------------- PROVEEDORES VIEWS -------------------#
-class ProveedorList(ListView):
+class ProveedorList(LoginRequiredMixin, ListView):
 
     model = Proveedor
     template_name = './Proveedores/proveedor_list.html'
     context_object_name = 'Proveedores'
 
-class ProveedorDetail(DetailView):
+class ProveedorDetail(LoginRequiredMixin, DetailView):
 
     model = Proveedor
     template_name = './Proveedores/proveedor_detail.html'
     context_object_name = 'Proveedor'
 
-class ProveedorCreate(CreateView):
+class ProveedorCreate(LoginRequiredMixin, CreateView):
     model = Proveedor
     template_name = './Proveedores/proveedor_create.html'
     form_class: ProveedorFormulario
@@ -62,14 +101,14 @@ class ProveedorCreate(CreateView):
     success_url = '/listaProveedores/' 
 
 
-class ProveedorUpdate(UpdateView):
+class ProveedorUpdate(LoginRequiredMixin, UpdateView):
 
     model = Proveedor
     template_name = './Proveedores/proveedor_update.html'
     fields = ('__all__')
     success_url = '/listaProveedores/'
 
-class ProveedorDelete(DeleteView):
+class ProveedorDelete(LoginRequiredMixin, DeleteView):
 
     model = Proveedor
     template_name = './Proveedores/proveedor_delete.html'
@@ -77,19 +116,19 @@ class ProveedorDelete(DeleteView):
 
 #------------------- CLIENTES VIEWS -------------------#
 
-class ClienteList(ListView):
+class ClienteList(LoginRequiredMixin, ListView):
 
     model = Cliente
     template_name = './Clientes/cliente_list.html'
     context_object_name = 'Clientes'
 
-class ClienteDetail(DetailView):
+class ClienteDetail(LoginRequiredMixin, DetailView):
 
     model = Cliente
     template_name = './Clientes/cliente_detail.html'
     context_object_name = 'Cliente'
 
-class ClienteCreate(CreateView):
+class ClienteCreate(LoginRequiredMixin, CreateView):
     model = Cliente
     template_name = './Clientes/cliente_create.html'
     form_class: ClienteFormulario
@@ -97,14 +136,14 @@ class ClienteCreate(CreateView):
     success_url = '/listaClientes/' 
 
 
-class ClienteUpdate(UpdateView):
+class ClienteUpdate(LoginRequiredMixin, UpdateView):
 
     model = Cliente
     template_name = './Clientes/Cliente_update.html'
     fields = ('__all__')
     success_url = '/listaClientes/'
 
-class ClienteDelete(DeleteView):
+class ClienteDelete(LoginRequiredMixin, DeleteView):
 
     model = Cliente
     template_name = './Clientes/cliente_delete.html'
@@ -113,19 +152,19 @@ class ClienteDelete(DeleteView):
 
 # ---------------------------Articulos 
 
-class ArticuloList(ListView):
+class ArticuloList(LoginRequiredMixin, ListView):
 
     model = Articulo
     template_name = './Articulos/Articulo_list.html'
     context_object_name = 'Articulos'
 
-class ArticuloDetail(DetailView):
+class ArticuloDetail(LoginRequiredMixin, DetailView):
 
     model = Articulo
     template_name = './Articulos/Articulos_detail.html'
     context_object_name = 'Articulos'
 
-class ArticuloCreate(CreateView):
+class ArticuloCreate(LoginRequiredMixin, CreateView):
     model = Articulo
     template_name = './Articulos/Articulo_create.html'
     form_class: ArticuloFormulario
@@ -133,14 +172,14 @@ class ArticuloCreate(CreateView):
     success_url = '/listaArticulos/' 
 
 
-class ArticuloUpdate(UpdateView):
+class ArticuloUpdate(LoginRequiredMixin, UpdateView):
 
     model = Articulo
     template_name = './Articulos/Articulo_update.html'
     fields = ('__all__')
     success_url = '/listaArticulos/'
 
-class ArticuloDelete(DeleteView):
+class ArticuloDelete(LoginRequiredMixin, DeleteView):
 
     model = Articulo
     template_name = './Articulos/Articulo_delete.html'
