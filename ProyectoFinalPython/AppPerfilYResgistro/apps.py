d1c35594eb8028dd9c85921c92a73e2f2eb29761
Jorge Alonso Rojas Perez <jarojasp@ccss.sa.cr>
2022-08-16 19:17:53 -0600

    Apps

---
+++ ProyectoFinalPython/AppPerfilYResgistro/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppperfilyresgistroConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AppPerfilYResgistro'
--- ProyectoFinalPython/ProyectoFinalPython/urls.py
+++ ProyectoFinalPython/ProyectoFinalPython/urls.py
@@ -6,4 +6,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AppProyectoFinalPython.urls')),
+    path('', include('AppCatalogoArticulos.urls')),
+    path('', include('AppPerfilYResgistro.urls')),
 ]
