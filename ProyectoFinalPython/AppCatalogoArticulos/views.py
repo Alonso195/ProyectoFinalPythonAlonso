d106e0532bc0ef66908ccc2915a22eda4ac72bfb
Jorge Alonso Rojas Perez <jarojasp@ccss.sa.cr>
2022-08-23 18:27:17 -0600

    detalle articulo

--- ProyectoFinalPython/AppCatalogoArticulos/views.py
+++ ProyectoFinalPython/AppCatalogoArticulos/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from AppProyectoFinalPython.models import Articulo
+
 # Create your views here.
 
 
 def lista_articulos(self):
         
     return render(self, "lista-articulos.html")
+
+
+
+def detalleArticulo(request, id):    
+            
+    articulo = Articulo.objects.get(id=id)    
+    
+    return render(request, "detalle-articulo.html", {"Articulo": articulo, "id": articulo.id})
